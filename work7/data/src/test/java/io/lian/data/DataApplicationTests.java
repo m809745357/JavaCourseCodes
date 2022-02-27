c827d8860f893656ee52cc2b3c3d9d2192545890
沈益飞 <m809745357@gmail.com>
2022-02-27 22:49:48 +0800

    feat(work7): 读写分离-动态切换数据源版本1.0

---
+++ work7/data/src/test/java/io/lian/data/DataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lian.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
